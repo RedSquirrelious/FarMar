2da99960a2693f7dcf38658b2666019cc600b436
Sassa Kitka <sassa.kitka@gmail.com>
2016-09-06 18:28:36 -0700

    removed class variable, new all? method not using class variable

--- lib/farmar_market.rb
+++ lib/farmar_market.rb
@@ -1,3 +1,5 @@
+require 'csv'
+require_relative 'farmar_shared_methods'
 # FarMar::Market
 
 # Each individual market has many vendors associated with it. The FarMar::Market data, in order in the CSV, consists of:
@@ -18,40 +20,78 @@
 # Additional FarMar::Market Methods
 
 # vendors: returns a collection of FarMar::Vendor instances that are associated with the market by the market_id field.
+module FarMar
 
-class FarMar::Market
-	attr_reader :name, :market_id, :instance_collector
 
-	@@instance_collector = []
-	
-	def initialize(details_hash)
-		@market_id = details_hash[:market_id]
-		@name = details_hash[:name]
-		@address = details_hash[:address]
-		@city = details_hash[:city]
-		@county = details_hash[:county]
-		@state = details_hash[:state]
-		@zip = details_hash[:zip]
-		@@instance_collector << self
-	end
 
-	def self.all?  #returns a collection of instances, representing all of the objects described in the CSV
-		return @@instance_collector
-	end
+	class Market
+	extend SharedMethods
+		attr_reader :name, :id, :instance_collector, :vendor_id
 
-	def self.find(id) #returns an instance of the object where the value of the id field in the CSV matches the passed parameter.
-	 	@@instance_collector.each do |x| 
-			if x.market_id  == id
-				return x
-			end
+
+		
+		def initialize(details_hash)
+			@id = details_hash[:id]
+			@name = details_hash[:name]
+			@address = details_hash[:address]
+			@city = details_hash[:city]
+			@county = details_hash[:county]
+			@state = details_hash[:state]
+			@zip = details_hash[:zip]
+			@@instance_collector << self
+			# self.class.send(:extend, SharedMethods)
 		end
-		return nil
+
+		# def self.all?  #returns a collection of instances, representing all of the objects described in the CSV
+		# 	return @@instance_collector
+		# end
+
+		# def self.find(id) #returns an instance of the object where the value of the id field in the CSV matches the passed parameter.
+		#  	@@instance_collector.each do |x| 
+		# 		if x.market_id  == id
+		# 			return x
+		# 		end
+		# 	end
+		# 	return nil
+		# end
+
+	#TAKES CSV DATA TO ADD NEW MARKETS
+		# def self.add_markets
+		# 	multiple_markets = []
+
+	 #  		CSV.foreach('support/markets.csv', 'r') do |line|
+	 #  			multiple_markets << self.new(id: line[0], name: line[1], address: line[2], city: line[3], county: line[4], state: line[5], zip: line[6])
+	  	
+		# 	end
+		# 	return multiple_markets
+		# end
+
+		def self.all?  #returns a collection of instances, representing all of the objects described in the CSV
+			multiple_instances = []
+
+				CSV.foreach('support/markets.csv', 'r') do |line|
+				multiple_instances << self.new(id: line[0], name: line[1], address: line[2], city: line[3], county: line[4], state: line[5], zip: line[6])
+
+				end
+			return multiple_instances
+		end
+
+		def self.vendors(id) #returns a collection of FarMar::Vendor instances that are associated with the market by the market_id field.
+
+		 	CSV.foreach('support/vendors.csv', 'r').each do |line| 
+				# puts line [3]
+				if line[3]  == id
+					puts "i'm in the loop!"
+					print line
+				end
+			end
+			return nil
+		end
+
+
 	end
-
-
-
-	def vendors #returns a collection of FarMar::Vendor instances that are associated with the market by the market_id field.
-	end
-
-
 end
+
+# Totoro = FarMar::Market.add_markets
+
+# puts Totoro.length
