3c106a426a5e7c52fa41cb73ac5651f2db952978
Sassa Kitka <sassa.kitka@gmail.com>
2016-09-07 13:40:46 -0700

    added working shared method for classes for all? method

--- lib/farmar_shared_methods.rb
+++ lib/farmar_shared_methods.rb
@@ -17,16 +17,34 @@ module SharedMethods
 		return nil
 	end
 
-	# def all?(thing)  #returns a collection of instances, representing all of the objects described in the CSV
-	# 	multiple_instances = []
 
- #  		CSV.foreach("support/#{thing}.csv", 'r') do |line|
- #  			multiple_instances << self.new(id: line[0], name: line[1], address: line[2], city: line[3], county: line[4], state: line[5], zip: line[6])
-  	
-	# 	end
-	# 	return multiple_instances
-	# end
+	def all?  #returns a collection of instances, representing all of the objects described in the CSV
+		multiple_instances = []
+  		# class_case = self.class	
+  			case 
+  			when FarMar::Market
+				CSV.foreach('support/markets.csv', 'r') do |line|
+  					multiple_instances << self.new(id: line[0], name: line[1], address: line[2], city: line[3], county: line[4], state: line[5], zip: line[6])
+				end
 
+  			when FarMar::Vendor
+				CSV.foreach('support/vendors.csv', 'r') do |line|
+  					multiple_instances << self.new(id: line[0], name: line[1], num_of_employees: line[2], market_id: line[3])
+				end
+
+  			when FarMar::Product
+				CSV.foreach('support/products.csv', 'r') do |line|
+  					multiple_instances << self.new(id: line[0], name: line[1], vendor_id: line[2])
+				end
+
+  			when FarMar::Sale
+				CSV.foreach('support/sales.csv', 'r') do |line|
+  					multiple_instances << self.new(id: line[0], amount: line[1].to_i, purchase_time: line[2], vendor_id: line[3], product_id: line[4])
+				end
+			end
+
+		return multiple_instances
+	end
 
 
 end
