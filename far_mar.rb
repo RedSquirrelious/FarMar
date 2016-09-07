7ce723c20870b0f085f669ff5741af85bc8cece6
Sassa Kitka <sassa.kitka@gmail.com>
2016-09-06 18:30:52 -0700

    method calls added to test new class methods outside of minitest

--- far_mar.rb
+++ far_mar.rb
@@ -9,4 +9,22 @@ require_relative 'lib/farmar_market'
 require_relative 'lib/farmar_vendor'
 require_relative 'lib/farmar_product'
 require_relative 'lib/farmar_sale'
+
+require_relative 'lib/farmar_shared_methods'
 # ...require all needed classes
+
+Totoro = FarMar::Market.all?
+
+puts Totoro.length
+
+puts Totoro[0]
+
+
+
+
+# puts FarMar::Market.vendors("1")
+
+puts FarMar::Market.find("1")
+
+
+# puts FarMar::Market.all?
