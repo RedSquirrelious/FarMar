aae861b5a3a3501dd350d3c3807270f3e725f523
Sassa Kitka <sassa.kitka@gmail.com>
2016-09-08 15:21:18 -0700

    includes all required methods, self.all?, find, vendors

--- lib/farmar_market.rb
+++ lib/farmar_market.rb
@@ -14,50 +14,57 @@ require_relative 'farmar_shared_methods'
 
 # For each of the data classes build the following methods:
 
-### self.all: returns a collection of instances, representing all of the objects described in the CSV
-### self.find(id): returns an instance of the object where the value of the id field in the CSV matches the passed parameter.
+#[DONE, TESTED] self.all: returns a collection of instances, representing all of the objects described in the CSV
+#[DONE in SharedMethods module, TESTED] self.find(id): returns an instance of the object where the value of the id field in the CSV matches the passed parameter.
 
 # Additional FarMar::Market Methods
 
-# vendors: returns a collection of FarMar::Vendor instances that are associated with the market by the market_id field.
+#[DONE] vendors: returns a collection of FarMar::Vendor instances that are associated with the market by the market_id field.
 module FarMar
 
 
 
 	class Market
 	extend SharedMethods
-		attr_reader :name, :id, :vendor_id
+		attr_reader :name, :id
 
+			def initialize(details_hash)
+				@id = details_hash[:id]
+				@name = details_hash[:name]
+				@address = details_hash[:address]
+				@city = details_hash[:city]
+				@county = details_hash[:county]
+				@state = details_hash[:state]
+				@zip = details_hash[:zip]
 
-		
-		def initialize(details_hash)
-			@id = details_hash[:id]
-			@name = details_hash[:name]
-			@address = details_hash[:address]
-			@city = details_hash[:city]
-			@county = details_hash[:county]
-			@state = details_hash[:state]
-			@zip = details_hash[:zip]
-
-		end
-
-
-		def self.vendors(id) #returns a collection of FarMar::Vendor instances that are associated with the market by the market_id field.
-
-		 	CSV.foreach('support/vendors.csv', 'r').each do |line| 
-				# puts line [3]
-				if line[3]  == id
-					puts "i'm in the loop!"
-					print line
-				end
 			end
-			return nil
+
+
+#DONE			
+		def self.all?  #returns a collection of instances, representing all of the objects described in the CSV
+			multiple_instances = []
+			CSV.foreach('support/markets.csv', 'r') do |line|
+				multiple_instances << self.new(id: line[0], name: line[1], address: line[2], city: line[3], county: line[4], state: line[5], zip: line[6])
+			end
+			return multiple_instances
 		end
 
 
+#the find(id) method is shared across all the classes - can be found in the SharedMethods module
+
+
+
+#DONE
+		def vendors #returns a collection of FarMar::Vendor instances that are associated with the market by the market_id field.
+		vendor_list = []
+		list = FarMar::Vendor.all?
+		list.each do |vendor|
+			if vendor.market_id == @id
+				puts vendor.name
+				vendor_list << vendor
+			end
+		end
+		return vendor_list
+	end
 	end
 end
-
-# Totoro = FarMar::Market.add_markets
-
-# puts Totoro.length
