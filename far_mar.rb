bba3ebba2cd8d797a96ec1adf5a55a706e05cb63
Sassa Kitka <sassa.kitka@gmail.com>
2016-09-09 11:57:01 -0700

    includes self testing

--- far_mar.rb
+++ far_mar.rb
@@ -2,7 +2,7 @@
 # gems your project needs
 require 'csv'
 require 'awesome_print'
-require 'chronic'
+# require 'chronic'
 # our namespace module
 module FarMar; end
 
@@ -13,5 +13,45 @@ require_relative 'lib/farmar_product'
 require_relative 'lib/farmar_sale'
 
 require_relative 'lib/farmar_shared_methods'
-# ...require all needed classes
+require_relative 'lib/farmar_shared_instance_methods'
+# # ...require all needed classes
+
+# ruby = FarMar::Market.all?
+
+# puts ruby[42].id
+# puts ruby[42].name
+# puts ruby[42].vendors.to_s + "i'm a market vendor"
+
+# coriander = FarMar::Vendor.all?
+
+# puts coriander[4].id
+# puts coriander[4].name
+# puts coriander[4].products
+# puts coriander[4].sales
+
+# salamander = FarMar::Sale.all?
+
+# puts salamander[0].id
+# puts salamander[0].amount
+# puts salamander[0].products
+# # puts salamander[0].sales
+
+# cylinder = FarMar::Product.all?
+
+# puts cylinder[0].id
+# puts cylinder[0].name
+# puts cylinder[0].sales[0].amount.to_s + "is how much someone spent"
+# # puts cylinder[42].vendors
+
+# puts FarMar::Product.find(43).name
+
+		# totoro = FarMar::Market.all?
+		# cat_bus = totoro[50].id
+		# puts cat_bus
+		# neko_basu = totoro[50].name
+		# puts neko_basu
+		# puts FarMar::Market.find(cat_bus)
+
+
+
 
