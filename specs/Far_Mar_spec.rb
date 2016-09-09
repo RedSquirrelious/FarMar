62c394902029e936440891ba8c63b35a612a0982
Sassa Kitka <sassa.kitka@gmail.com>
2016-09-09 10:37:00 -0700

    achieved nearly 100 percent coverage, one line not covered because of extreme date range used in test

--- specs/Far_Mar_spec.rb
+++ specs/Far_Mar_spec.rb
@@ -8,7 +8,9 @@ describe 'Testing FarMar' do
 
 #MARKET SPECS
 	it 'Must Tell Me if a Market Instance Was Made' do
-		expect( FarMar::Market.new(name: "Ballard").name ).must_equal("Ballard")
+		bugs_bunny = FarMar::Market.all?
+		wabbit = bugs_bunny[50]
+		expect( wabbit.class ).must_equal(FarMar::Market)
 	end
 
 	it 'Must Return All Market Instances' do
@@ -25,60 +27,132 @@ describe 'Testing FarMar' do
 	end
 
 	it 'Must Return the List of Vendors at a Given Market' do
-		bugs_bunny = FarMar::Market.all?
-		elmer_fudd = bugs_bunny[10]
-		yosemite_sam = FarMar::Vendor.all?
-		expect( yosemite_sam ).must_include(elmer_fudd.vendors)
+		elmer_fudd = FarMar::Market.all?
+		wabbit = elmer_fudd[50]
+		expect( wabbit.vendors ).wont_equal(1)
 	end
 
 # #PRODUCT SPECS
 
-# 	it 'Must Assign the Right Information to a New Product' do
-# 		expect( FarMar::Product.new(name: "Thingamajig").name ).must_equal("Thingamajig")
-# 	end
+	it 'Must Assign the Right Information to a New Product' do
+		expect( FarMar::Product.new(name: "Thingamajig").name ).must_equal("Thingamajig")
+	end
 
-# 	it 'Must Return All Product Instances' do
-# 		FarMar::Product.new({product_id: 789, name: "Thingamajig", vendor_id: 1001} )
-# 		expect( FarMar::Product.all?.length ).wont_equal(0)
-# 	end
+	it 'Must Return All Product Instances' do
+		# FarMar::Product.new({product_id: 789, name: "Thingamajig", vendor_id: 1001} )
+		expect( FarMar::Product.all?.length ).wont_equal(0)
+	end
 
-# 	# it 'Must Look Up the Name of a Product if Given an ID' do
-# 	# 	FarMar::Product.new({id: 789, name: "Thingamajig", vendor_id: 1001} )
-# 	# expect( FarMar::Product.find(789).name ).must_equal("Thingamajig")
-# 	# end
+	it 'Must Show the Number of Sales of a Given Product' do
+		rodent = FarMar::Product.all?
+		# hamster = rodent[35].id
+		guinea_pig = rodent[2]
+		expect( guinea_pig.sales ).wont_equal( 0 )
+		# expect( guinea_pig.number_of_sales ).wont_equal( 0 )
+		expect( guinea_pig.number_of_sales ).must_equal( guinea_pig.sales.length)
+	end
+
+	it 'Must Look Up a Products Sales' do
+		cat = FarMar::Product.all?
+		maine_coon = cat[0]
+		expect( maine_coon.sales ).wont_be_empty
+		expect( maine_coon.sales[0].class ).must_equal(FarMar::Sale)
+	end
  
+
+	 it 'Must Look Up Products Sold by a Given Vendor' do
+		bird = FarMar::Vendor.all?
+		canary = bird[1].id
+		expect( FarMar::Product.by_vendor(canary) ).wont_equal(0)
+		expect( FarMar::Product.by_vendor(canary)[0].class).must_equal( FarMar::Product)
+	end
+
 # #SALE SPECS
 
-# 	it 'Must Create New Sale' do
-# 		expect( FarMar::Sale.new(purchase_time: 1300).purchase_time ).must_equal(1300)
-# 	end
-# 	it 'Must Return All Sale Instances' do
-# 		FarMar::Sale.new({id: 789, amount: 25, purchase_time: 1200, vendor_id: 1001, product_id: 789})
-# 		expect( FarMar::Sale.all?.length ).wont_equal(0)
-# 	end
+	it 'Must Find Sales that Fall in a Range of Purchase Times' do
+#current range of sales are in 2013
+		beginning_time = '11/8/3050 11:00' 
+		ending_time = '11/11/3050 13:00'
+		expect( FarMar::Sale.between(beginning_time, ending_time)).must_be_empty
+	end
 
-# 	# it 'Must Look Up the Amount of a Sale if Given an ID' do
-# 	# 	FarMar::Sale.new({id: 1953, amount: 25, purchase_time: 1200, vendor_id: 1001, product_id: 789})
-# 	# expect( FarMar::Sale.find(1953).amount).must_equal(25)
-# 	# end
+	it 'Must Return All Sale Instances' do
+		instrument = FarMar::Sale.all?
+		harp = instrument[42]
+		expect( instrument.length ).wont_equal(0)
+		expect( harp.class ).must_equal(FarMar::Sale)
+	end
+
+	it 'Must Return the Vendor for a Given Sale' do
+		melon = FarMar::Sale.all?
+		cantaloupe = melon[1000]
+		expect( cantaloupe.vendor ).wont_equal(0)
+		expect( cantaloupe.vendor[0].class ).must_equal(FarMar::Vendor)
+	end
+
+	it 'Must Show the Products Sold in a Given Sale' do
+		furniture = FarMar::Sale.all?
+		loveseat = furniture[42]
+		expect( loveseat.product ).wont_equal(0)
+		expect( loveseat.product[0].class ).must_equal( FarMar::Product )
+	end
+
 
 # #VENDOR SPECS
 
-# 	it 'Must Tell Me if a Vendor Instance Was Made' do
-# 		expect( FarMar::Vendor.new(name: "Stuff and Things LLC").name ).must_equal("Stuff and Things LLC")
-# 	end
+	it 'Must Tell Me if a Vendor Instance Was Made' do
+		expect( FarMar::Vendor.new(name: "Stuff and Things LLC").name ).must_equal("Stuff and Things LLC")
+	end
 
-# 	it 'Must Return All Vendor Instances' do
-# 		FarMar::Vendor.new({id: 1001, name: "Stuff and Things LLC", num_of_employees: 2, market_id: 123})
-# 		expect( FarMar::Vendor.all?.length ).wont_equal(0)
-# 	end
+	it 'Must Return All Vendor Instances' do
+		computer = FarMar::Vendor.all?
+		mac = computer[42]
+		expect( mac.class ).must_equal( FarMar::Vendor )
 
-# 	# it 'Must Look Up the Number of a Vendor\'s Employees if Given an ID' do
-# 	# 	FarMar::Vendor.new({id: 1001, name: "Stuff and Things LLC", num_of_employees: 2, market_id: 123})
-# 	# expect( FarMar::Vendor.find(1001).num_of_employees ).must_equal(2)
-# 	# end
-# 	# it 'Must Raise an ArgumentException if given a non-Fixnum' do
-# 	# 	expect( proc { FizzBuzzX::FizzBuzzX.fizzbuzzx("Something") } ).must_raise ArgumentError
-# 	# end
+	end
 
+	it 'Must Look Up the Vendors that Sell at a Given Market' do
+		vegetable = FarMar::Vendor.all?
+		carrot = vegetable[100]
+		expect( carrot.market ).wont_be_empty
+	end
+
+	it 'Must Return Products Sold by a Given Vendor' do
+		mineral = FarMar::Vendor.all?
+		granite = mineral[300]
+		expect( granite.products.length ).wont_equal(0)
+		expect (granite.products[0].class ).must_equal( FarMar::Product )
+	end
+
+	it 'Must Give the Amount of Total Revenue for a Vendor' do
+		fruit = FarMar::Vendor.all? 
+		durian = fruit[250]
+		range = 0..5000000000000
+		expect( range ).must_include( durian.revenue )
+	end
+
+	it 'Must Look Up Sales By a Given Vendor' do
+		pet = FarMar::Vendor.all?
+		chinchilla = pet[42]
+		expect( chinchilla.sales ).wont_be_empty
+		expect( chinchilla.sales[0].class ).must_equal( FarMar::Sale )
+	end
+
+	it 'Must Look Up Products Sold By a Given Vendor' do
+		dog = FarMar::Vendor.all?
+		sammoyed = dog[42]
+		expect( sammoyed.products ).wont_be_empty
+		expect( sammoyed.products[0].class ).must_equal(FarMar::Product)
+	end
+
+	it 'Must Show All the Vendors Who Sell at a Given Market' do
+		holiday = FarMar::Market.all?
+		halloween = holiday[42].id
+		expect( FarMar::Vendor.by_market(halloween) ).wont_equal(0)
+		expect( FarMar::Vendor.by_market(halloween)[0].class ).must_equal( FarMar::Vendor )
+	end
+	# it 'Must Raise an ArgumentException if given a non-Fixnum' do
+	# 	expect( proc { FizzBuzzX::FizzBuzzX.fizzbuzzx("Something") } ).must_raise ArgumentError
+	# end
 end
+
